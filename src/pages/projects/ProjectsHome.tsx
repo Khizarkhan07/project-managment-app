dfbceaec134b3f155017adc0e8ce7a67b58dd4ea
khizarkhan07 <khizar062@gmail.com>
2020-12-16 14:57:15 +0500

    project header component; user project refactor; homepage refactor;

--- src/pages/projects/ProjectsHome.tsx
+++ src/pages/projects/ProjectsHome.tsx
@@ -3,6 +3,7 @@ import {useProjectContext} from "../../contexts/projectContext";
 import { Divider, PageHeader, Button, Descriptions,  Avatar, Image } from 'antd';
 import {ProjectWrapper} from "./project.styles";
 import { Link } from 'react-router-dom';
+import ProjectHeader from "../../components/projectHeader";
 
 const ProjectsHome = () =>{
     const{state, dispatch} = useProjectContext();
@@ -10,42 +11,10 @@ const ProjectsHome = () =>{
     const renderProjects = useMemo(()=> {
         return state.projects.map((project)=> {
             return(
-                <ProjectWrapper>
-                    <PageHeader
-                        key={project.id}
-                        ghost={false}
-                        title={project.name}
-
-                    >
-                        <Descriptions size="small" column={3}>
-                            <Descriptions.Item label="Created By">{project.createdBy}</Descriptions.Item>
-                            <Descriptions.Item label="id">
-                                <a>{project.id}</a>
-                            </Descriptions.Item>
-                            <Descriptions.Item label="Creation Time">{new Date(project.createdAt).toLocaleDateString()}</Descriptions.Item>
-                            <Descriptions.Item label="Effective Time">{new Date(project.createdAt).toLocaleDateString()}</Descriptions.Item>
-                            <Descriptions.Item label="Tech stack">{
-                                project.tech.map((tech: any)=> {
-                                    return tech + " "
-                                })
-                            }</Descriptions.Item>
-
-                            <Descriptions.Item label="Team">
-                                {project.team.map((member: any)=> {
-                                    return (
-                                        <Avatar>{member.username.substr(0,1).toUpperCase()}</Avatar>
-                                    )
-                                })}
-                            </Descriptions.Item>
-
-                            <Descriptions.Item label="Description">{project.description}</Descriptions.Item>
-                        </Descriptions>
-                    </PageHeader>
-                </ProjectWrapper>
-
+                <ProjectHeader project={project}/>
             )
         })
-    }, [])
+    }, [state.projects])
 
     return (
         <div>
--- src/pages/projects/UserProjects.tsx
+++ src/pages/projects/UserProjects.tsx
@@ -4,6 +4,7 @@ import {getAuthenticatedUser} from "../../utils";
 import {ProjectWrapper} from "./project.styles";
 import {Avatar, Button, Descriptions, PageHeader} from "antd";
 import {Link} from "react-router-dom";
+import ProjectHeader from "../../components/projectHeader";
 
 const UserProjects = () => {
     const {state, dispatch} = useProjectContext();
@@ -16,43 +17,7 @@ const UserProjects = () => {
         if(state.userProjects){
             return state.userProjects.map((project)=> {
                 return(
-                    <ProjectWrapper>
-                        <PageHeader
-                            key={project.id}
-                            ghost={false}
-                            title={project.name}
-                            extra={[
-                                <Button key="1" type="primary">
-                                    <Link to = {`/project/reviews/${project.id}`}>Reviews</Link>
-                                </Button>,
-                            ]}
-                        >
-                            <Descriptions size="small" column={3}>
-                                <Descriptions.Item label="Created By">{project.createdBy}</Descriptions.Item>
-                                <Descriptions.Item label="id">
-                                    <a>{project.id}</a>
-                                </Descriptions.Item>
-                                <Descriptions.Item label="Creation Time">{new Date(project.createdAt).toLocaleDateString()}</Descriptions.Item>
-                                <Descriptions.Item label="Effective Time">{new Date(project.createdAt).toLocaleDateString()}</Descriptions.Item>
-                                <Descriptions.Item label="Tech stack">{
-                                    project.tech.map((tech: any)=> {
-                                        return tech + " "
-                                    })
-                                }</Descriptions.Item>
-
-                                <Descriptions.Item label="Team">
-                                    {project.team.map((member: any)=> {
-                                        return (
-                                            <Avatar>{member.username.substr(0,1).toUpperCase()}</Avatar>
-                                        )
-                                    })}
-                                </Descriptions.Item>
-
-                                <Descriptions.Item label="Description">{project.description}</Descriptions.Item>
-                            </Descriptions>
-                        </PageHeader>
-                    </ProjectWrapper>
-
+                    <ProjectHeader project={project}/>
                 )
             })
         }
